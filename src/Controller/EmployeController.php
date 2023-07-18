29f0728c327a848a21a33362e30b02f85355e957
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-18 13:33:47 +0200

    feat: add findBy() in index and show() method + add comments about findBy() function and route redirection for show() method

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Employe;
+use Doctrine\ORM\EntityRepository;
+use App\Repository\EmployeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EmployeController extends AbstractController
 {
     #[Route('/employe', name: 'app_employe')]
-    public function index(): Response
+    // public function index(EntityManagerInterface $entityManager): Response
+    public function index(EmployeRepository $employeRepository): Response
     {
+        // $employes = $entityManager->getRepository(Employe::class)->findAll();
+        // $employes = $employeRepository->findAll();
+        /* Le premier argument permet de choisir ce que l'on souhaite afficher, cela équivaut à un 
+        "SELECT"
+        Le second argument du findBy équivaut à "SELECT * FROM employe ORDER BY nom ASC" */
+        $employes = $employeRepository->findBy([], ["nom" => "ASC"]);
         return $this->render('employe/index.html.twig', [
-            'controller_name' => 'EmployeController',
+            'employes' => $employes,
+        ]);
+    }
+
+    // Pour afficher un élément par rapport à un indentifiant, il faut ajouter dans l'url de la route '/{id}'
+    #[Route('/employe/{id}', name: 'show_employe')]
+    public function show(Employe $employe): Response 
+    {
+        return $this->render('employe/show.html.twig', [
+            'employe' => $employe
         ]);
     }
 }
