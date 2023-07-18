87653e416b95e8ccc79e2f8a72b258a584653097
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-07-18 16:48:20 +0200

    feat: add new_edit form and delete method + add comments about new form method, the route redirection for edit and delete() method

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -7,6 +7,7 @@ use App\Form\EntrepriseType;
 use Doctrine\ORM\EntityRepository;
 use App\Repository\EntrepriseRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,6 +23,7 @@ class EntrepriseController extends AbstractController
     {
         // $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
         // $entreprises = $entrepriseRepository->findAll();
+        
         /* Le premier argument filtre ce que l'on souhaite afficher, cela équivaut à un 
         "SELECT * FROM entreprise WHERE ville = 'STRASBOURG' ORDER BY raisonSociale ASC"
         Le second argument du findBy équivaut à "SELECT * FROM entreprise ORDER BY raisonSociale ASC" */
@@ -30,6 +32,48 @@ class EntrepriseController extends AbstractController
         return $this->render('entreprise/index.html.twig', [
             'entreprises' => $entreprises
         ]);
+    }
+
+    #[Route('/entreprise/new', name: 'new_entreprise')]
+    // Route pour éditer une entreprise par son id
+    #[Route('/entreprise/{id}/edit', name: 'edit_entreprise')]
+    public function new_edit(Entreprise $entreprise = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // La valeur de $entreprise est null par défaut, si l'entreprise n'existe pas, on met une nouvelle Entreprise comme valeur
+        if (!$entreprise) {
+            $entreprise = new Entreprise();
+        }
+        
+        $form = $this->createForm(EntrepriseType::class, $entreprise);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() retient les valeurs soumises
+            // mais la variable $entreprise est également mise à jour
+             $entreprise = $form->getData();
+            
+            /* persist dit à Doctrine de sauvegarder dans une instance l'objet en argument,
+            équivaut à un prepare PDO */
+            $entityManager->persist($entreprise);
+
+            // Exécute la requête, équivaut à execute PDO
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_entreprise');
+        }
+
+        return $this->render('entreprise/new.html.twig', [
+            'formAddEntreprise' => $form,
+            'edit' => $entreprise->getId()
+        ]);
+    }
+
+    #[Route('/entreprise/{id}/delete', name: 'delete_entreprise')]
+    public function delete(Entreprise $entreprise, EntityManagerInterface $entityManager) {
+        $entityManager->remove($entreprise);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_entreprise');
     }
 
     // Pour afficher un élément par rapport à un indentifiant, il faut ajouter dans l'url de la route '/{id}'
